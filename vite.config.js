625577c9fe6a3ca398f004cd2e7a15d940652f5f
lichangrong10 <3809421280@qq.com>
2026-09-20 11:31:30 +0800

    ip更改（用此版本）

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://192.168.9.160:3001',
+        target: 'http://192.168.9.112:3001',
         changeOrigin: true
       }
     }
